ab9318de6a5d516cd0ca708b5c23ec2d6183741c
ericmiedema <ericmiedema@gmail.com>
2018-11-28 17:22:30 -0800

    update path

--- lapply_and_sapply/initLesson.R
+++ lapply_and_sapply/initLesson.R
@@ -1,6 +1,6 @@
 # Path to installed lesson
 .lessonpath <- file.path(path.package("swirl"), "Courses", 
-                         "R_Programming_Alt", "lapply_and_sapply")
+                         "Data_Analytics", "lapply_and_sapply")
 # Path to dataset
 .datapath <- file.path(.lessonpath, "flag.data.txt")
 # Load dataset
